44c01d0d5f31e1c90bcca0c073015e0ae98b9430
Mijeong Kwon <84905321+kmj-1616@users.noreply.github.com>
2025-09-12 14:49:50 +0900

    [D1] Title: 연속한 1의 개수, Time: 64 ms, Memory: 55,680 KB -BaekjoonHub

--- SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
+++ SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -1,23 +1,17 @@
 T = int(input())
-
-for test_case in range(1, T+1):
+for tc in range(1, T+1):
     N = int(input())
     arr = input()
 
-    # 1의 개수 기록할 변수
-    count = 0
-    # 1의 개수 중 최댓값 기록할 변수
-    max_count = 0
-    # 수열을 순회 
-    for i in arr: 
-        # 1이면 개수 추가
-        if i == '1': 
-            count += 1
-            # 현재 카운트보다 최대 카운트가 크면 갱신 
-            if count > max_count:
-                max_count = count 
-        # 1이 아니면 (0이면) 횟수 세지 않음 
-        else: 
-            count = 0
+    cnt = 0
+    max_cnt = 0
+    for i in range(N):
+        # 1이 나오면 길이 세고 최댓값 갱신
+        if arr[i] == '1':
+            cnt += 1
+            max_cnt = max(max_cnt, cnt)
+        # 0 나와서 끊기면 카운트 초기화 
+        else:
+            cnt = 0
 
-    print(f"#{test_case} {max_count}")
+    print(f'#{tc} {max_cnt}')
