4351a4a015a336f6e486ba137dbd398931454146
Mijeong Kwon <84905321+kmj-1616@users.noreply.github.com>
2025-09-17 23:23:09 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(1, T+1):
+    N = int(input())
+    dp = [0]*(11)
+
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    for i in range(4, 11):
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+        
+    print(dp[N])
