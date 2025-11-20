b828143cc9082bbc529cd6038adf2bf25caf79ef
Mijeong Kwon <84905321+kmj-1616@users.noreply.github.com>
2025-11-20 23:17:14 +0900

    [Silver IV] Title: 수열, Time: 120 ms, Memory: 40900 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.py
@@ -0,0 +1,10 @@
+N = int(input())
+lst = list(map(int, input().split()))
+dp1, dp2 = [1]*N, [1]*N
+
+for i in range(1, N):
+    if lst[i] <= lst[i-1]:
+        dp1[i] = max(dp1[i], dp1[i-1]+1)
+    if lst[i] >= lst[i-1]:
+        dp2[i] = max(dp2[i], dp2[i-1]+1)
+print(max(max(dp1), max(dp2)))
