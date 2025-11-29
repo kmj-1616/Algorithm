29d94f0e29901f46437bb50d176f257457060515
Mijeong Kwon <84905321+kmj-1616@users.noreply.github.com>
2025-11-29 22:06:51 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,9 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+s = input()
+
+for i in alphabet:
+    if i in s:
+        print(s.count(i), end=' ')
+    else:
+        print(0, end=' ')
