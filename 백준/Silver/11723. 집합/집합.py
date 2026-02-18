763ec5c39779c0fc50005d0b2d34ed049065599c
Mijeong Kwon <84905321+kmj-1616@users.noreply.github.com>
2026-02-18 15:21:53 +0900

    [Silver V] Title: 집합, Time: 2156 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,33 +1,37 @@
 import sys
+
 input = sys.stdin.readline
 
-M = int(input())
-S = set()   # 집합은 중복된 원소가 없는 set
+def func_set():
+    m = int(input())
+    s = set()
 
-for _ in range(M):
-    txt = input().split()
-    cmd = txt[0]
+    for _ in range(m):
+        line = input().split()
+        command = line[0]
+        
+        # 숫자 연산 
+        if len(line) > 1:
+            x = int(line[1])
+            
+            if command == "add":
+                s.add(x)
+            elif command == "remove":
+                s.discard(x) 
+            elif command == "check":
+                print(1 if x in s else 0)
+            elif command == "toggle":
+                if x in s:
+                    s.remove(x)
+                else:
+                    s.add(x)
+        
+        # 숫자 필요 없는 연산
+        else:
+            if command == "all":
+                # 1부터 20까지의 집합으로 바꾸기
+                s = set(range(1, 21))
+            elif command == "empty":
+                s.clear()
 
-    if cmd == "add":
-        x = int(txt[1])
-        S.add(x)
-    elif cmd == "remove":
-        x = int(txt[1])
-        S.discard(x)  # remove는 S에 x가 없으면 오류가 난다
-    elif cmd == "check":
-        # 미리 x 변수를 만들면 check와 toggle의 경우에 indexerror가 난다
-        x = int(txt[1])
-        if x in S:
-            print(1)
-        else:
-            print(0)
-    elif cmd == "toggle":
-        x = int(txt[1])
-        if x in S:
-            S.remove(x)
-        else:
-            S.add(x)
-    elif cmd == "all":
-        S = set(range(1, 21))
-    elif cmd == "empty":
-        S = set()
+func_set()
